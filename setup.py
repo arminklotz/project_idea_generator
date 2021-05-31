b74299503602e86da8b08f56b02c831e1691cd2d
Armin Klotz <armin.klotz@hotmail.com>
2021-05-31 16:43:39 +0100

    Initial Push

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='project_idea_generator',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
-      install_requires=requirements,
-      test_suite='tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/project_idea_generator-run'],
-      zip_safe=False)
+setup(
+    name='project_idea_generator',
+    version="1.0",
+    description="Project Description",
+    packages=find_packages(),
+    install_requires=requirements,
+    test_suite='tests',
+    # include_package_data: to install data from MANIFEST.in
+    include_package_data=True,
+    scripts=['scripts/project_idea_generator-run', 'scripts/project_idea_generate'],
+    zip_safe=False)
